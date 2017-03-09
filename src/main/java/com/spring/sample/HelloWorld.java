25609f637b5674fd7781cdc6283a9cde667e9093
qomarullah <qomarullah@telkomsel.co.id>
2017-03-09 17:59:30 +0700

    initial commit

---
+++ src/main/java/com/spring/sample/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.spring.sample;
+
+public class HelloWorld {
+	private String name;
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void printHello() {
+		System.out.println("Hello ! " + name);
+	}
+}
